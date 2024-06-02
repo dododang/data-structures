e889d4d0f92c46be4667270758b71de42f2cf49f
dodongtak <94369467+dododang@users.noreply.github.com>
2024-06-02 22:36:01 +0900

    Add files via upload

--- week13/adjList.c
+++ week13/adjList.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include "adjList.h"
 
 void createGraph(graphType* g) {
 	int v;
 	g->n = 0;
-	for (v = 0; v < MAX_VERTEX; v++)
+	for (v = 0; v < MAX_VERTEX; v++) {
 		g->adjList_H[v] = NULL;
+		g->visited[v] = FALSE;
+	}
 }
 
 void insertVertex(graphType* g, int v) {
@@ -34,7 +36,7 @@ void print_adjList(graphType* g) {
 	int i;
 	graphNode* p;
 	for (i = 0; i < g->n; i++) {
-		printf("\n\t\t 정점 %c의 인접 리스트", i + 65);
+		printf("\n\t\t정점 %c의 인접 리스트", i + 65);
 		p = g->adjList_H[i];
 		while (p) {
 			printf(" -> %c", p->vertex + 65);
